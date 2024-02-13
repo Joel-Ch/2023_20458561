edb630707901c45a806ec61e66e9e9e93ce4c5ef
Joel C <joelchiappetti@gmail.com>
2024-02-13 10:33:50 +0000

    Doxygen?

---
+++ Worksheets/5/adder.h
@@ -0,0 +1,16 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/**  @file  
+ * This file contains the declarations of all exported functions.
+*/
+
+
+/** Adding Functions
+ * This function adds two integers
+ * @param a The first integer to be added
+ * @param b The second integer to be added
+ * @return The sum of a and b
+*/
+int add(int a, int b);
+#endif
