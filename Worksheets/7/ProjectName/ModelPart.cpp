fb2437bdcde89ce24418e5d273e91fd883caa455
Joel C <joelchiappetti@gmail.com>
2024-03-05 15:29:42 +0000

    nothing here

--- Worksheets/7/ProjectName/ModelPart.cpp
+++ Worksheets/7/ProjectName/ModelPart.cpp
@@ -11,7 +11,6 @@
 ModelPart::ModelPart(const QList<QVariant>& data, ModelPart* parent )
     : m_itemData(data), m_parentItem(parent) {
 
-    /* You probably want to give the item a default colour */
 }
 
 
